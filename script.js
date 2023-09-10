f051e99af7b11a6b7eb31615a749770f3e1eb63c
Manguriu <101741844+Manguriu@users.noreply.github.com>
2023-09-10 11:34:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -15,9 +15,20 @@ function weekUpdate() {
 }
 
 function timeUpdate() {
-  const currentUTCTime = new Date().getTime();
-  document.querySelector('[data-testid="currentUTCTime"]').textContent =
-    currentUTCTime;
+  setInterval(() => {
+    const currentTime = new Date();
+    const hours = currentTime.getUTCHours().toString().padStart(2, "0");
+    const minutes = currentTime.getUTCMinutes().toString().padStart(2, "0");
+    const seconds = currentTime.getUTCSeconds().toString().padStart(2, "0");
+    const milliseconds = currentTime
+      .getUTCMilliseconds()
+      .toString()
+      .padStart(3, "0");
+
+    const formattedTime = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+    document.querySelector('[data-testid="currentUTCTime"]').textContent =
+      formattedTime;
+  }, 100); 
 }
 
 weekUpdate();
