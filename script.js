21feb8f91bb117412ccdab26c9f19f217ec402a8
manguriu <brianmiruri983@gmail.com>
2023-09-10 12:01:18 +0300

    css

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//day
 function weekUpdate() {
   const weekDays = [
     "Sunday",
@@ -14,21 +15,17 @@ function weekUpdate() {
     weekDay;
 }
 
+//time
 function timeUpdate() {
-  setInterval(() => {
-    const currentTime = new Date();
-    const hours = currentTime.getUTCHours().toString().padStart(2, "0");
-    const minutes = currentTime.getUTCMinutes().toString().padStart(2, "0");
-    const seconds = currentTime.getUTCSeconds().toString().padStart(2, "0");
-    const milliseconds = currentTime
-      .getUTCMilliseconds()
-      .toString()
-      .padStart(3, "0");
-
-    const formattedTime = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+  const updateUTCTimeElement = () => {
+    const currentTime = new Date().getTime();
     document.querySelector('[data-testid="currentUTCTime"]').textContent =
-      formattedTime;
-  }, 100); 
+      currentTime;
+  };
+
+  updateUTCTimeElement();
+
+  setInterval(updateUTCTimeElement, 100);
 }
 
 weekUpdate();
